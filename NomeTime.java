bf030c3bb02234a73b6ead540efc3673f80601a5
verissimo <terceiroverissimo@gmail.com>
2023-03-22 21:46:37 -0300

    adicionando codigo em comentarios

--- NomeTime.java
+++ NomeTime.java
@@ -1,6 +1,24 @@
-class NomeTime {
+public class NomeTime {
     public static void main(String[] args){
         System.out.println("Verissimo");
         System.out.println("Sousa");
     }
 }
+
+/*
+Nome da classe diferente tem como resultado da compilação Diferente.class
+class Diferente {
+    public static void main(String[] args){
+        System.out.println("Verissimo");
+        System.out.println("Sousa");
+    }
+*/
+
+/* tudo maiúsculo
+PUBLIC CLASS NOMETIME {
+    PUBLIC STATIC VOID MAIN(STRING[] ARGS){
+        SYSTEM.OUT.PRINTLN("vERISSIMO");
+        SYSTEM.OUT.PRINTLN("SOUSA");
+    }
+}
+*/
